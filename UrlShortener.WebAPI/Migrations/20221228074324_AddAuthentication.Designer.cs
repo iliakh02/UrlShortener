9a302e260185af8d69eb3cde7b12855e83f68466
liakhivan <liakhivan21@email.com>
2022-12-28 10:04:55 +0200

    Add ASP.NET Core Identity to the project.

--- UrlShortener.WebAPI/Migrations/20221228074324_AddAuthentication.Designer.cs
+++ UrlShortener.WebAPI/Migrations/20221228074324_AddAuthentication.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UrlShortener.WebAPI.Models;
 
@@ -11,9 +12,11 @@ using UrlShortener.WebAPI.Models;
 namespace UrlShortener.WebAPI.Migrations
 {
     [DbContext(typeof(UrlShortenerDbContext))]
-    partial class UrlShortenerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221228074324_AddAuthentication")]
+    partial class AddAuthentication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- UrlShortener.WebAPI/Models/UrlShortenerDbContext.cs
+++ UrlShortener.WebAPI/Models/UrlShortenerDbContext.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace UrlShortener.WebAPI.Models
 {
-    public class UrlShortenerDbContext : DbContext
+    public class UrlShortenerDbContext : IdentityDbContext
     {
         public DbSet<Link> Links { get; set; }
         public UrlShortenerDbContext(DbContextOptions<UrlShortenerDbContext> options) : base(options)
